9f605227733488c18ecdd4e9932fa63da16336ca
Timothy Crosley <timothy.crosley@gmail.com>
2020-08-18 00:08:33 -0700

    Basic styling, ability to close with ctrl+q

--- quickpython/cli.py
+++ quickpython/cli.py
@@ -1,5 +1,43 @@
 from prompt_toolkit import Application
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.key_binding import KeyBindings
+
+kb = KeyBindings()
+
+@kb.add('c-q')
+def exit_(event):
+    """
+    Pressing Ctrl-Q will exit the user interface.
+
+    Setting a return value means: quit the event loop that drives the user
+    interface and return this value from the `Application.run()` call.
+    """
+    event.app.exit()
+    
+buffer1 = Buffer()  # Editable buffer.
+
+root_container = HSplit([
+    # One window that holds the BufferControl with the default buffer on
+    # the left.
+    Window(content=BufferControl(buffer=buffer1)),
+
+    # A vertical line in the middle. We explicitly specify the width, to
+    # make sure that the layout engine will not try to divide the whole
+    # width by three for all these windows. The window will simply fill its
+    # content by repeating this character.
+    Window(height=1, char='|'),
+
+    # Display the text 'Hello world' on the right.
+    Window(content=FormattedTextControl(text='Hello world')),
+], style="bg:#0000AA fg:#AAAAAA bold")
+
+layout = Layout(root_container)
+
+app = Application(layout=layout, full_screen=True, key_bindings=kb)
 
 def start():
-    app = Application(full_screen=True)
     app.run()
+    
