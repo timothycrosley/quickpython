b58c32cba4314369602c9d0f5b9608c3f047c91f
Timothy Crosley <timothy.crosley@gmail.com>
2020-08-18 22:57:17 -0700

    User interface improvements

--- quickpython/cli.py
+++ quickpython/cli.py
@@ -1,13 +1,16 @@
-from prompt_toolkit import Application
+from prompt_toolkit import Application, widgets
 from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout.containers import HSplit, VSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
-from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.widgets import TextArea, toolbars
+from prompt_toolkit.widgets.base import Box, Frame, Button
 
 kb = KeyBindings()
 
-@kb.add('c-q')
+
+@kb.add("c-q")
 def exit_(event):
     """
     Pressing Ctrl-Q will exit the user interface.
@@ -16,28 +19,58 @@ def exit_(event):
     interface and return this value from the `Application.run()` call.
     """
     event.app.exit()
+
+
+@kb.add('tab')
+def indent(event):
+    event.app.current_buffer.insert_text('    ')
+
+
+class MainEditor(TextEditor):
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.buffer.on_text_changed
+        
+    def format_code(self):
+        
+
+
 buffer1 = Buffer()  # Editable buffer.
 
-root_container = HSplit([
-    # One window that holds the BufferControl with the default buffer on
-    # the left.
-    Window(content=BufferControl(buffer=buffer1)),
-
-    # A vertical line in the middle. We explicitly specify the width, to
-    # make sure that the layout engine will not try to divide the whole
-    # width by three for all these windows. The window will simply fill its
-    # content by repeating this character.
-    Window(height=1, char='|'),
-
-    # Display the text 'Hello world' on the right.
-    Window(content=FormattedTextControl(text='Hello world')),
-], style="bg:#0000AA fg:#AAAAAA bold")
+root_container = HSplit(
+    [
+        Box(VSplit([Button(text="File"), Button(text="Edit")]), height=1, style="bg:#AAAAAA fg:black bold"),
+        Frame(
+            HSplit(
+                [
+                    # One window that holds the BufferControl with the default buffer on
+                    # the left.
+                    TextArea(scrollbar=True, wrap_lines=False, focus_on_click=True),
+                    # A vertical line in the middle. We explicitly specify the width, to
+                    # make sure that the layout engine will not try to divide the whole
+                    # width by three for all these windows. The window will simply fill its
+                    # content by repeating this character.
+                ],
+                style="bg:#0000AA fg:#AAAAAA bold",
+            ),
+            title="Untitled",
+            style="bg:#0000AA fg:#AAAAAA bold",
+        ),
+        Frame(
+            TextArea(),
+            title="Immediate",
+            height=10,
+            style="bg:#0000AA fg:#AAAAAA bold",
+        )
+    ]
+)
 
 layout = Layout(root_container)
 
-app = Application(layout=layout, full_screen=True, key_bindings=kb)
+app = Application(layout=layout, full_screen=True, key_bindings=kb, mouse_support=True)
+
 
 def start():
     app.run()
-    
