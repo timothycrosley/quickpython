8ee7f71fcafaa44f3c164ee41b01e77e6a7735c9
Timothy Crosley <timothy.crosley@gmail.com>
2020-08-19 20:34:40 -0700

    isort format code as typed

--- quickpython/cli.py
+++ quickpython/cli.py
@@ -1,9 +1,12 @@
+import isort
+
 from prompt_toolkit import Application, widgets
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout.containers import HSplit, VSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.utils import Event
 from prompt_toolkit.widgets import TextArea, toolbars
 from prompt_toolkit.widgets.base import Box, Frame, Button
 
@@ -26,16 +29,22 @@ def indent(event):
     event.app.current_buffer.insert_text('    ')
 
 
-class MainEditor(TextEditor):
+class MainEditor(TextArea):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.buffer.on_text_changed
+        self.buffer.on_text_changed = Event(self.buffer, self.format_code)
         
-    def format_code(self):
+    def format_code(self, code):
+        end_position = self.buffer.text.rfind("\n", 0, self.buffer.cursor_position) + 1
+        code, rest = self.buffer.text[:end_position], self.buffer.text[end_position:]
+        formatted_code = isort.code(code)
+        difference = len(formatted_code) - len(code)
+        self.buffer.cursor_position += difference
+        self.buffer.text = formatted_code + rest
         
-
+        
 
 buffer1 = Buffer()  # Editable buffer.
 
@@ -47,7 +56,7 @@ root_container = HSplit(
                 [
                     # One window that holds the BufferControl with the default buffer on
                     # the left.
-                    TextArea(scrollbar=True, wrap_lines=False, focus_on_click=True),
+                    MainEditor(scrollbar=True, wrap_lines=False, focus_on_click=True),
                     # A vertical line in the middle. We explicitly specify the width, to
                     # make sure that the layout engine will not try to divide the whole
                     # width by three for all these windows. The window will simply fill its
