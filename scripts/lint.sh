29e55ba992d8bfd6176e9b2c9af22e86c5bcd66c
Timothy Crosley <timothy.crosley@gmail.com>
2020-09-12 23:53:46 -0700

    Update to latest version of cruft template

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy quickpython/
+poetry run mypy --ignore-missing-imports quickpython/
 poetry run isort --check --diff quickpython/ tests/
 poetry run black --check quickpython/ tests/
 poetry run flake8 quickpython/ tests/
