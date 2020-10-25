cdc55d43813e0b976f2e8d34d0fb65196d784bb8
Timothy Crosley <timothy.crosley@gmail.com>
2020-10-24 21:54:29 -0700

    Add clear command

--- quickpython/extensions.py
+++ quickpython/extensions.py
@@ -1,8 +1,18 @@
 import builtins
+import platform
+from subprocess import run
 
 
 def beep():
     print("\a", end="")
     
+
+def cls():
+    if platform.system().lower() == "windows":
+        run("cls")
+    else:
+        run("clear")
+    
     
 builtins.beep = beep
+builtins.cls = cls
