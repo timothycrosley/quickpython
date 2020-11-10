df140cdb4053cd9117559379b06cc6bb43210365
Timothy Crosley <timothy.crosley@gmail.com>
2020-11-09 22:38:15 -0800

    add main decorator extension

--- quickpython/extensions.py
+++ quickpython/extensions.py
@@ -14,5 +14,14 @@ def cls():
         run("clear")
 
 
-builtins.beep = beep
-builtins.cls = cls
+def main(function):
+    caller = inspect.stack()[1]
+    module = inspect.getmodule(caller[0])
+    if module.__name__ == "__main__":
+        function()
+    return function
+
+
+setattr(builtins, "beep", beep)
+setattr(builtins, "cls", cls)
+setattr(builtins, "main", main)
